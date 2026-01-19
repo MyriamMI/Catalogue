b30cabb4bb7e82a296aec9a9337c49cd1307a58d
NK-Flory <nkashama.flory@gmail.com>
2026-01-19 17:52:45 +0100

    New index +css

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-include __DIR__ . "/processCatalogue.php"; // prépare $produits
+include __DIR__ . "/processCatalogue.php";
+require_once __DIR__ . "/TypeDAO.php";
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -15,17 +17,13 @@ include __DIR__ . "/processCatalogue.php"; // prépare $produits
 
   <div class="grid">
     <?php foreach ($produits as $p): ?>
-      <?php
-        $img = $p->getImage();
-        if (substr($img, -4) !== ".png") {
-          $img .= ".png";
-        }
-        $src = "images/" . $img;
-      ?>
 
       <div class="card">
         <div class="card-img">
-          <img src="<?php echo $src; ?>" alt="<?php echo $p->getLibelle(); ?>">
+          <img
+            src="images/<?php echo $p->getImage(); ?>"
+            alt="<?php echo $p->getLibelle(); ?>"
+          >
         </div>
 
         <div class="card-body">
@@ -33,7 +31,9 @@ include __DIR__ . "/processCatalogue.php"; // prépare $produits
           <p class="card-desc"><?php echo $p->getDescription(); ?></p>
 
           <div class="card-footer">
-            <a class="btn" href="#">Voir</a>
+            <span class="type-label">
+              <?php echo TypeDAO::findById($p->getIdType())->getLibelle(); ?>
+            </span>
           </div>
         </div>
       </div>
