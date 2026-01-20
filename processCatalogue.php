a1b3c86ea33cc0142d71e5ff0a1534f4368d6cae
NK-Flory <nkashama.flory@gmail.com>
2026-01-20 18:02:21 +0100

    process cat + style

--- processCatalogue.php
+++ processCatalogue.php
@@ -1,6 +1,14 @@
 <?php
 require_once __DIR__ . "/MonPDO.php";
-require_once __DIR__ . "/classes/Produit.class.php";
 require_once __DIR__ . "/ProduitDAO.php";
+require_once __DIR__ . "/TypeDAO.php";
 
-$produits = ProduitDAO::findAll();
+$types = TypeDAO::findAll();
+
+$typeSelected = isset($_GET["type"]) ? $_GET["type"] : "tous";
+
+if ($typeSelected === "tous") {
+    $produits = ProduitDAO::findAll();
+} else {
+    $produits = ProduitDAO::findByType($typeSelected);
+}
